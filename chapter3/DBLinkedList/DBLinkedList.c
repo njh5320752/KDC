bb0019926808c6320e7fb42cd77b8d3e0d8c20d6
Felinus.jeong <felinus.jeong@windriver.com>
2016-04-01 19:17:41 +0900

    Modifty data type and sort function
    Signed-off-by: Felinus.jeong <felinus.jeong@windriver.com>

--- chapter3/DBLinkedList/DBLinkedList.c
+++ chapter3/DBLinkedList/DBLinkedList.c
@@ -6,10 +6,10 @@ struct _DList
 {
     DList *next;
     DList *prev;
-    int data;
+    void *data;
 };
 
-DList* d_list_append(DList *list, int data) {
+DList* d_list_append(DList *list, void *data) {
     DList *new_list;
     DList *last;
     new_list = (DList*) malloc(sizeof(DList));
@@ -30,7 +30,7 @@ DList* d_list_append(DList *list, int data) {
     }
 }
 
-DList* d_list_insert_first(DList* list, int data) {
+DList* d_list_insert_first(DList *list, void *data) {
     DList *new_list;
     new_list = (DList*) malloc(sizeof(DList));
     if (!new_list) {
@@ -75,7 +75,7 @@ int d_list_length(DList *list) {
     return lenght;
 }
 
-DList* d_list_remove_nth_with_data(DList* list, int data) {
+DList* d_list_remove_nth_with_data(DList *list, void *data, void(*free_data)(void *data)) {
     DList *tmp;
     printf("Remove data:%d\n", data);
     tmp = list;
@@ -92,6 +92,7 @@ DList* d_list_remove_nth_with_data(DList* list, int data) {
             if(list == tmp) {
                 list = list->next;
             }
+            free_data(tmp->data);
             free(tmp);
             break;
         }
@@ -108,7 +109,7 @@ void d_list_print_all_data(DList *list) {
     }
 }
 
-DList* d_list_insert(DList *list, int data, int n) {
+DList* d_list_insert(DList *list, void *data, int n) {
     DList *new_list;
     DList *tmp_list;
 
@@ -137,14 +138,14 @@ DList* d_list_insert(DList *list, int data, int n) {
     }
 }
 
-DList* d_list_nth_recursion(DList* list, int n) {
+DList* d_list_nth_recursion(DList *list, int n) {
     if (n <= 0) {
         return list;
     }
     return d_list_nth_recursion(d_list_next(list), n-1);
 }
 
-DList* d_list_nth_for(DList* list, int n) {
+DList* d_list_nth_for(DList *list, int n) {
     int i;
     for (i = 0; i < n && list != NULL; i++) {
         list = list->next;
@@ -152,7 +153,7 @@ DList* d_list_nth_for(DList* list, int n) {
     return list;
 }
 
-int d_list_get_data(DList* list) {
+void* d_list_get_data(DList *list) {
     if (!list) {
         printf("Can't get data");
         return 0;
@@ -160,7 +161,7 @@ int d_list_get_data(DList* list) {
     return list->data;
 }
 
-DList* d_list_remove_nth(DList* list, int n) {
+DList* d_list_remove_nth(DList *list, int ni, void(*free_data)(void *data)) {
     DList *remove;
     remove = d_list_nth_for(list, n);
     if (remove == NULL) {
@@ -177,11 +178,12 @@ DList* d_list_remove_nth(DList* list, int n) {
     if(list == remove) {
         list = list->next;
     }
+    free_data(remove->data);
     free(remove);
     return list;
 }
 
-DList* d_list_remove(DList *list) {
+DList* d_list_remove(DList *list, void(*free_data)(void *data)) {
     DList* remove;
 
     if (!list) {
@@ -197,21 +199,23 @@ DList* d_list_remove(DList *list) {
         list->next->prev = NULL;
     }
 	list = list->next;
+    free_data(remove->data);
     free(remove);
 	return list;
 }
 
-void d_list_free(DList *list) {
+void d_list_free(DList *listi, void(*free_data)(void *data)) {
     DList* remove;
     while(list) {
         remove = list;
         list = list->next;
+        free_data(remove->data)
         free(remove);
     }
     return;
 }
 
-int d_list_nth_with_data(DList *list, int data) {
+int d_list_nth_with_data(DList *list, void *data) {
     int i = 0;
     while (list) {
         if (list->data == data) {
@@ -223,29 +227,32 @@ int d_list_nth_with_data(DList *list, int data) {
     return -1;
 }
 
-void d_list_sort(DList* list, int(*comp)(int data1, int data2)) {
+void d_list_sort(DList* list, int(*comp)(void *data1, void *data2)) {
     int i, j, tmp;
+    bool is_switched = false;
     DList* node;
-
     if (!(list) || !(list->next)) {
         printf("Can't sort the list\n");
         return;
     }
     i = d_list_length(list);
-	for (i = i -1; i > 0; i--) {
+    for (i = i -1; i > 0; i--) {
         node = list;
-		for (j = 0; j < i; j++) {
-			if (comp(node->data, node->next->data)) {
-				tmp = node->next->data;
-				node->next->data = node->data;
-				node->data = tmp;
-			}
+        for (j = 0; j < i; j++) {
+            if (comp(node->data, node->next->data)) {
+                switch_node(node, node->next)                
+                is_switched = true;
+            }
             node = node->next;
-		}
+        }
+        if (!is_switched) {
+            break;
+        } 
+        is_switched = false;
     }
 }
 
-void d_list_insert_sort(DList* list, int(*comp)(int data1, int data2)) {
+void d_list_insert_sort(DList* list, int(*comp)(void *data1, void *data2)) {
     int i, n, remember;
     DList* remember_node;
     DList* tmp;
@@ -268,4 +275,25 @@ void d_list_insert_sort(DList* list, int(*comp)(int data1, int data2)) {
         }
         remember_node = remember_node->next;
     }
+}
+
+DList* switch_node(DList* node1, DList* node2) {
+    DList *tmp1 = node1->next;
+    DList *tmp2 = node2->prev;
+
+    node1->next = node2->next;
+    if (node2->next) {
+        node2->next->prev = node1;
+    }
+
+    node2->prev = node1->prev;
+
+    if (node1->prev) {
+        node1->prev->next = node2;
+    }
+
+    node1->prev = tmp2;
+    node2->next = tmp1;
+    tmp1->prev = node2;
+    tmp2->next = node1;
 }
--- chapter3/DBLinkedList/DBLinkedList.h
+++ chapter3/DBLinkedList/DBLinkedList.h
@@ -4,22 +4,22 @@
 typedef struct _DList DList;
 
 /* Doubly linked lists  */
-DList* d_list_append(DList *list, int data);
+DList* d_list_append(DList *list, void *data);
 DList* d_list_last(DList *list);
 int d_list_length(DList *list);
-DList* d_list_remove_nth_with_data(DList* list, int data);
-DList* d_list_insert(DList *list, int data, int n);
-DList* d_list_nth_recursion(DList* list, int n);
-DList* d_list_nth_for(DList* list, int n);
+DList* d_list_remove_nth_with_data(DList* list, void *data, void(*free_data)(void *data));
+DList* d_list_insert(DList *list, void *data, int n);
+DList* d_list_nth_recursion(DList *list, int n);
+DList* d_list_nth_for(DList *list, int n);
 #define d_list_next(list) ((list) ? (((DList *)(list))->next) : NULL)
-DList* d_list_remove_nth(DList* list, int index);
-void d_list_print_all_data(DList* list);
-void d_list_free(DList* list);
-int d_list_nth_with_data(DList* list, int data);
-int d_list_get_data(DList* list);
-DList* d_list_insert_first(DList* list, int data);
-void d_list_sort(DList* list, int(*comp)(int data1, int data2));
-DList* d_list_remove(DList* list);
+DList* d_list_remove_nth(DList *list, int index, void(*free_data)(void *data));
+void d_list_print_all_data(DList *list);
+void d_list_free(DList* list, void(*free_data)(void *data));
+int d_list_nth_with_data(DList *list, void *data);
+int d_list_get_data(DList *list);
+DList* d_list_insert_first(DList* list, void *data);
+void d_list_sort(DList *list, int(*comp)(void *data1, void *data2));
+DList* d_list_remove(DList *list, void(*free_data)(void *data));
 DList* d_list_first(DList *list);
-void d_list_insert_sort(DList* list, int(*comp)(int data1, int data2));
+void d_list_insert_sort(DList *list, int(*comp)(void *data1, void *data2));
 #endif
