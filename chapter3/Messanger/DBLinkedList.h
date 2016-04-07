38e70f695685f9e4e8bb2ef383e5ed5517daa2b0
Felinus.jeong <felinus.jeong@windriver.com>
2016-04-07 19:37:40 +0900

    implement server and makefile
    Signed-off-by: Felinus.jeong <felinus.jeong@windriver.com>

---
+++ chapter3/Messanger/DBLinkedList.h
@@ -0,0 +1,28 @@
+#ifndef __DB_LINKED_LIST_H__
+#define __DB_LINKED_LIST_H__
+
+typedef struct _DList DList;
+
+/* Doubly linked lists  */
+DList* d_list_append(DList *list, void *data);
+DList* d_list_last(DList *list);
+int d_list_length(DList *list);
+DList* d_list_remove_nth_with_data(DList* list, void *data, void(*free_data)(void *data));
+DList* d_list_insert(DList *list, void *data, int n);
+DList* d_list_nth_recursion(DList *list, int n);
+DList* d_list_nth_for(DList *list, int n);
+#define d_list_next(list) ((list) ? (((DList *)(list))->next) : NULL)
+DList* d_list_remove_nth(DList *list, int index, void(*free_data)(void *data));
+void d_list_print_all_data(DList *list, void(*print_data)(void* data));
+void d_list_free(DList* list, void(*free_data)(void *data));
+int d_list_nth_with_data(DList *list, void *data);
+void* d_list_get_data(DList *list);
+DList* d_list_insert_first(DList* list, void *data);
+DList* d_list_bubble_sort(DList *list, int(*comp)(void *data1, void *data2));
+DList* d_list_remove(DList *remove, void(*free_data)(void *data));
+DList* d_list_first(DList *list);
+DList* d_list_insert_sort(DList *list, int(*comp)(void *data1, void *data2));
+DList* d_list_switch_node(DList* node1, DList* node2);
+DList* d_list_prepend_node(DList* sorted_node, DList* insert_node, DList *list); 
+DList* d_list_delete(DList *remove);
+#endif
--- chapter3/Messanger/server.c
+++ chapter3/Messanger/server.c
@@ -2,28 +2,41 @@
 #include <sys/socket.h>
 #include <sys/un.h>
 #include <stdlib.h>
+#include "DBLinkedList.h"
 
 char *socket_path = "";
 
 int main(int argc, char *argv[]) {
     struct sockaddr_un addr;
-    char buf[100];
     int servfd,cl,rc;
-    
+    int option = 1;
     if (argc > 1) {
         socket_path = argv[1];
     }
 
-    if ((fd = socket(AF_UNIX, SOCK_STREA,0)) == -1) {
+    if ((servfd = socket(AF_UNIX, SOCK_STREAM,0)) == -1) {
         perror("socket error");
         exit(-1);
     }
 
-    if (fd < 0) {
-        error(1, errno, "Unix Domain Socket Error()");
-    }
     memset(&addr, 0, sizeof(addr));
     addr.sun_family = AF_UNIX;
-    strncpy(addr.sun_path, "socket", sizeof(addr.sun_path)-1);
-    bind(fd, (struct sockaddr*)&addr, sizeof(addr));
+    strncpy(addr.sun_path, socket_path, sizeof(addr.sun_path)-1);
+    if (setsockopt(servfd, SOL_SOCKET, SO_REUSEADDR, &option, sizeof(option)) < 0) {
+        perror("setsockopt failed");
+        close(servfd);
+        exit(-1);
+    }
+
+    if (bind(servfd, (struct sockaddr*)&addr, sizeof(addr)) == -1) {
+        perror("bind error");
+        exit(-1);
+    }
+
+   if (listen(servfd, 0) == -1) {
+        perror("listen error");
+        exit(-1);
+    }
+
+    close(servfd);
 }
